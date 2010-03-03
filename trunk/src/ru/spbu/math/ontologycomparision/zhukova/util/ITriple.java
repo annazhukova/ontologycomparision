a45c27d0605d8607806722e590a28e0087f3cc47
zhutchok <zhutchok@f7394c78-104b-11df-8588-0bb593d8ec56>
2010-03-03 00:12:16 +0000

    clear all

--- trunk/src/ru/spbu/math/ontologycomparision/zhukova/util/ITriple.java
+++
@@ -1,9 +0,0 @@
-package ru.spbu.math.ontologycomparision.zhukova.util;
-
-/**
- * @author Anna Zhukova
- */
-public interface ITriple<F, S, T> extends IPair<F, S> {
-    
-    T getThird();
-}
--- trunk/src/ru/spbu/math/ontologycomparision/zhukova/visualisation/ui/commands/ICommand.java
+++
@@ -1,9 +0,0 @@
-package ru.spbu.math.ontologycomparision.zhukova.visualisation.ui.commands;
-
-
-public interface ICommand {
-    
-    public void doIt();
-
-    public void undo();
-}
